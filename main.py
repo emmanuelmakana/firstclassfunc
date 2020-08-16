37401fbd801f1e7b517962777505d343c2eed0d9
emmanuel makana <emmanuelekesa430@gmail.com>
2020-08-16 11:31:28 +0000

    Closure work simple

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-def square(x):
-  return x*x
+def logger(msg):
 
-def my_map(func, arg_list):
-  result = []
-  for i in arg_list:
-    result.append(func(i))
-  return result
+  def log_message():
+    print('Log:', msg)
+  return log_message
 
-squares = my_map(square, [1,2,3,4,5])
-print(squares)
-print(my_map)
+log_hi = logger('Hi!')
+log_hi()
+
+def sum(a, b):
+
+  def addnum():
+    print(a+b)
+  return addnum
+summation = sum(20,20)
+summation()
